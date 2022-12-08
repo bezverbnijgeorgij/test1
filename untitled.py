1b602c57d96e9c2ef1a1d8aa4064e13118241907
bezverbnijgeorgij <120141709+bezverbnijgeorgij@users.noreply.github.com>
2022-12-09 00:31:11 +0200

    Add files via upload

---
+++ untitled.py
@@ -0,0 +1,26 @@
+replacelist(ls,l+2*n+2,x+2*n+2)
+		replacelist(ls,x+2*n+2,m*n+x+2*n+2)
+		
+		replacelist(ls,l+2*n+1,x+2*n+1)
+		replacelist(ls,x+2*n+1,m*n+x+2*n+1)
+		
+		replacelist(ls,l+2*n+0,x+2*n+0)
+		replacelist(ls,x+2*n+0,m*n+x+2*n+0)
+		
+		replacelist(ls,l+n+2,x+n+2)
+		replacelist(ls,x+n+2,m*n+x+n+2)
+		
+		replacelist(ls,l+n+1,x+n+1)
+		replacelist(ls,x+n+1,m*n+x+n+1)
+		
+		replacelist(ls,l+n+0,x+n+0)
+		replacelist(ls,x+n+0,m*n+x+n+0)
+		
+		replacelist(ls,l+2,x+2)
+		replacelist(ls,x+2,m*n+x+2)
+		
+		replacelist(ls,l+1,x+1)
+		replacelist(ls,x+1,m*n+x+1)
+		
+		replacelist(ls,l,x)
+		replacelist(ls,x,m*n+x)
